1b1fabf6a6b67defb8092dfaf270611e63b9221b
Ezequiel Oliveira Barboza <132308977+Ezeki-el@users.noreply.github.com>
2023-08-18 23:11:10 -0300

    Add files via upload

--- Integrando HTML com JavaScript/main.js
+++ Integrando HTML com JavaScript/main.js
@@ -1,12 +1,29 @@
-form =  document.getElementById("Form deposito");
+form =  document.getElementById('form-deposito');
 
-function validanome(nomeCompleto) {
+function validaNome(nomeCompleto) {
     const nomeComoArray = nomeCompleto.split(' ');
     return nomeComoArray.length >= 2;
 }
 
-form.addEventListener("submit", function(e) {
+form.addEventListener('submit', function(e) {
+    let formEValido = false;
     e.preventDefaut();
+
+    const nomeBeneficiario = document.getElementById ('nome-beneficiario');
+    const numeroContaBeneficiario = document.getElementById ('numero-conta');
+    const valorDeposito = document.getElementById('valor-deposito');
+    const mensagemSucesso = 'Montante de: $(ValorDeposito) depo para o cliente: $(nomeBeneficiario) - conta: $(numeroContaBeneficiario)';
+
+    formEValido = validaNome(nomeBeneficiario.value)
+    if (formEValido) {
+        alert(mensagemSucesso);
+
+        nomeBeneficiario.value = '';
+        numeroContaBeneficiario.value = '';
+        valorDeposito.value = '';
+    } else {
+        alert("O nome não está completo"); 
+    }
 })
 
 console,log(form);
